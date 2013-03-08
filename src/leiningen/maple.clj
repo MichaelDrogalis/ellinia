6f83a30e16e1d1170dd7f9d8badc0c0a45dc4531
Michael Drogalis <mjd3089@rit.edu>
2013-03-08 11:01:02 -0500

    Downloading the image and plopping it in the right directory.

--- src/leiningen/maple.clj
+++ src/leiningen/maple.clj
@@ -13,6 +13,7 @@
   (vector (str "http://www.perioncorner.com/cache/images/Mob/" id ".img/")))
 
 (defn download-images! [entity id]
+  (println "Fetching the images from Perion Corner...")
   (apply sh (concat (wget entity) (mob id))))
 
 (defn all-images [dir]
@@ -22,21 +23,28 @@
   ["zip" "all.zip" "-r" (all-images (resource-dir entity))])
 
 (defn zip-images! [entity]
+  (println "Zipping the images for the Sprite Generator...")
   (apply sh (zip entity)))
 
 (defn stylesheet [entity]
-  (str "resources/public/stylesheets/" entity))
+  (str "resources/public/stylesheets/" entity ".css"))
 
 (defn create-css-contents! [css entity]
+  (println "Generating the CSS file...")
   (spit (stylesheet entity) css))
 
+(defn download-sprite-sheet! [entity link]
+  (println "Downloading the sprite sheet...")
+  (apply sh ["curl" "-o" (str (resource-dir entity) "/character.png") link]))
+
 (defn download-sprites-and-css! [entity]
+  (println "Uploading the images to the Sprite Generator...")
   (taxi/set-driver! {:browser :firefox} "http://spritegen.website-performance.org")
   (taxi/input-text "#class-prefix" (str entity "-"))
   (taxi/select-option "#build-direction" {:value "horizontal"})
   (taxi/input-text "#path" (str (System/getProperty "user.dir") "/all.zip"))
   (taxi/submit ".submit")
-  (taxi/click ".download")
+  (download-sprite-sheet! entity (.getAttribute (:webelement (taxi/element ".download")) "href"))
   (create-css-contents! (.getText (:webelement (taxi/element "#result form div textarea"))) entity))
 
 (defn maple
